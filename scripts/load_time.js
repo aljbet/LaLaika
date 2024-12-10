d204f1b706f6cd4527b79fe8c9ed22f2dedb64b6
aljbet <esivannikova3@gmail.com>
2024-12-10 15:05:06 +0300

    load time

---
+++ scripts/load_time.js
@@ -0,0 +1,8 @@
+(() => {
+    const startTime = Date.now();
+    window.addEventListener("load", () => {
+        const endTime = Date.now();
+        const loadTime = endTime - startTime;
+        document.getElementById("load_time").innerText = `Время загрузки страницы: ${loadTime} мс.`;
+    });
+})();
