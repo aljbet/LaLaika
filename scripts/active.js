12b02f610e055cb4c6b0fe72a83dbbe838584bb9
aljbet <esivannikova3@gmail.com>
2024-12-10 16:27:42 +0300

    different button colors

---
+++ scripts/active.js
@@ -0,0 +1,13 @@
+(() => {
+    const currentPage = window.location.pathname.split('/').pop();
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let items = document.getElementsByClassName("instrument_class");
+        for (let i = 0; i < items.length; i++) {
+            if (items.item(i).getAttribute("href") === currentPage) {
+                items.item(i).classList.add("active");
+                break;
+            }
+        }
+    });
+})();
